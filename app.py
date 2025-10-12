34bdb241001068e962ca0f8c7bfd5f558b269452
Vishnu Anand <vishnuanand2000@gmail.com>
2025-10-12 18:23:54 -0400

    Added Embedding Function

--- app.py
+++ app.py
@@ -3,3 +3,16 @@ from dotenv import load_dotenv
 import chromadb
 from openai import OpenAI
 from chromadb.utils import embedding_functions
+
+
+# Load environment variables
+load_dotenv()
+
+# Get API Key from environment variables
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Embedding Function - Allows us to create embeddings for our text data
+openai_embedding_functions = embedding_functions.OpenAIEmbeddings(
+    api_key=openai_api_key,
+    model_name="text-embedding-3-small"
+    )
